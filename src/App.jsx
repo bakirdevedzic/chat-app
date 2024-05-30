6d2159c5d303566752bfbad159dfc7b85da170b7
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-31 00:48:10 +0200

    Implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import ProtectedRoute from "./services/ProtectedRoute";
 
 function App() {
-  return <div>Initial</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route
+          path="/chat/:id"
+          element={
+            <ProtectedRoute>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
