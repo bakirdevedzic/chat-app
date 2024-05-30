b88df859f6a6a7a5d24555237f92184effcca98e
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-31 01:19:51 +0200

    set tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-black": "#1D2024",
+        "primary-gray": "#666666",
+        "primary-white": "#ebf0f4",
+      },
+      height: {
+        screen: "100dvh",
+      },
+      screens: {
+        xl: { max: "1380px" },
+        md: { min: "901px", max: "1200px" },
+        sm: { max: "900px" },
+        us: { max: "600px" },
+      },
+    },
   },
   plugins: [],
 };
