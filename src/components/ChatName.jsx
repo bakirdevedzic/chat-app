c505e1f33f27c0803cff6b091e5aff21679df41f
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-01 00:46:01 +0200

    implement chat name

---
+++ src/components/ChatName.jsx
@@ -0,0 +1,9 @@
+function ChatName() {
+  return (
+    <div className="h-[4rem] bg-white mb-2 pl-12 flex items-center align-middle">
+      <p className="text-xl font-bold ">Michael Phelps</p>
+    </div>
+  );
+}
+
+export default ChatName;
