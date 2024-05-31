2eb6676eea47ac3cf2d963b7ef63c334a27742d0
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-05-31 02:16:38 +0200

    implement sidebar

---
+++ src/components/SidebarComponent.jsx
@@ -0,0 +1,17 @@
+function SidebarComponent({
+  showSidebar,
+  sidebarHidden,
+  sidebarVisible,
+  setShowSidebar,
+}) {
+  let classNameBase =
+    "h-full bg-white row-span-full col-[1/2] px-[1.5rem] py-[1.5rem] flex flex-col border-solid border-0 border-r border-slate-200  fixed top-0 left-0 z-10 w-[300px] overflow-y-auto transition-[margin-left] ease-in-out duration-500";
+  let className = "";
+  if (sidebarVisible || (sidebarHidden && showSidebar))
+    className = classNameBase;
+  if (sidebarHidden && !showSidebar) className = classNameBase + " ml-[-300px]";
+
+  return <div className={className}>smth</div>;
+}
+
+export default SidebarComponent;
