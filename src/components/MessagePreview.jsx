ce680df3b3bdb4e110092ab51aa70fcc80857559
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-01 03:59:59 +0200

    change message preview lenght

--- src/components/MessagePreview.jsx
+++ src/components/MessagePreview.jsx
@@ -1,13 +1,15 @@
 const MessagePreview = ({
   name = "bakir",
-  message = "hello, how are you",
+  message = "hello, are you good my friend??",
   time = "15:43",
 }) => {
   return (
-    <div className="flex items-start justify-between  p-4 pl-[1.5rem] hover:bg-slate-300 hover:cursor-pointer">
+    <div className="flex items-start justify-between  p-4 pl-[1.5rem] hover:bg-primary-indigo hover:bg-opacity-20 hover:cursor-pointer">
       <div>
         <div className="font-bold text-gray-900">{name}</div>
-        <div className="text-gray-700">{message}</div>
+        <div className="text-gray-700">
+          {message.length > 25 ? message.slice(0, 25) + "..." : message}
+        </div>
       </div>
       <div className="text-sm text-primary-indigo">{time}</div>
     </div>
