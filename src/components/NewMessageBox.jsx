e42c4224a04d0b6714760f671b1f7dd33484e85c
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-01 03:54:33 +0200

    style message input tab

--- src/components/NewMessageBox.jsx
+++ src/components/NewMessageBox.jsx
@@ -2,14 +2,14 @@ import { IoIosSend } from "react-icons/io";
 
 function NewMessageBox() {
   return (
-    <div className="h-full bg-white flex items-center justify-center">
-      <div className="flex items-center w-[90%]  bg-white rounded-lg px-4 py-2">
+    <div className="h-full bg-white flex items-center justify-center border-t">
+      <div className="flex items-center w-[90%]  bg-white rounded-lg px-4 py-2 ">
         <input
           type="text"
           placeholder="Send message"
-          className="border-2 outline-none bg-gray-200 rounded-lg w-full h-14 py-3 px-4 mr-2"
+          className="border-2 outline-none bg-gray-200 rounded-lg w-full h-14 py-3 px-4 mr-2 focus:ring-[2px] focus:ring-primary-indigo"
         />
-        <div className="text-3xl flex justify-center items-center text-white w-12 h-12 bg-blue-700 rounded-full">
+        <div className="text-3xl flex justify-center items-center text-white w-12 h-12 bg-indigo-700 rounded-full">
           <IoIosSend />
         </div>
       </div>
