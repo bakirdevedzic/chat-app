e6a90737b1549b9b2a4c21d4846c766c1c609ec3
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-04 05:03:44 +0200

    implement login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+  }
+  return (
+    <div className="bg-gray-50 text-[#333]">
+      <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
+        <div className="max-w-md w-full border py-8 px-6 rounded border-gray-300 bg-white">
+          <h2 className="text-center text-3xl font-extrabold">
+            Log in to your account
+          </h2>
+          <form className="mt-10 space-y-4">
+            <div>
+              <input
+                name="email"
+                type="email"
+                required
+                className="w-full text-sm px-4 py-3 rounded outline-none border-2 focus:border-blue-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <input
+                name="password"
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full text-sm px-4 py-3 rounded outline-none border-2 focus:border-blue-500"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="!mt-10">
+              <button
+                type="button"
+                className="w-full py-2.5 px-4 text-sm rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                onClick={handleSubmit}
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
