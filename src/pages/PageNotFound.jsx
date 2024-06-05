4ea7e641a9d6e4e3c6f2b4ad95fbf4b17938c241
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-05 10:13:42 +0200

    implement page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,20 @@
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="h-[calc(100dvh)] flex items-center flex-col overflow-auto">
+      <section className="flex items-center h-screen p-16">
+        <div className="container flex flex-col items-center ">
+          <div className="flex flex-col gap-6 max-w-md text-center">
+            <h2 className="font-extrabold text-9xl text-gray-600 ">
+              <span className="sr-only">Error</span>404
+            </h2>
+            <p className="text-2xl md:text-3xl font-bold">
+              Sorry, we couldn&apos;t find this page.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default PageNotFound;
