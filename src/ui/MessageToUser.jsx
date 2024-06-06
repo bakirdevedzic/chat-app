ea7520f2f2727be903ee26c007044973c8ec6b9c
bakirdevedzic <devedzic.bakir2@gmail.com>
2024-06-06 09:41:02 +0200

    fix responsiveness bug

--- src/ui/MessageToUser.jsx
+++ src/ui/MessageToUser.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { mainContext } from "../context/MainContext";
+import { PiChatsCircleBold } from "react-icons/pi";
+
 function MessageToUser({ message }) {
+  const { setShowSidebar, setLeaveChat } = useContext(mainContext);
   return (
-    <div className="h-full flex justify-center items-center align-middle text-2xl font-bold text-gray-600 text-center">
+    <div className="h-full flex flex-col justify-center items-center align-middle text-2xl font-bold text-gray-600 text-center">
       {message}
+      <div
+        className="text-2xl text-primary-indigo hidden sm:block us:block"
+        onClick={() => setShowSidebar(true)}
+      >
+        <PiChatsCircleBold />
+      </div>
     </div>
   );
 }
